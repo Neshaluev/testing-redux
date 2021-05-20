3948c722895675865ca581949cba980996230d65
Neshaluev <31486387+Neshaluev@users.noreply.github.com>
2021-05-20 23:15:04 +0300

    Update tetsUtils.js

--- Utils/tetsUtils.js
+++ Utils/tetsUtils.js
@@ -1,4 +1,8 @@
 import checkPropTypes from "check-prop-types";
+import { createStore, applyMiddleware } from "redux";
+
+import rootReducer from "../src/reducers";
+import { middlewares } from "../src/configureStore";
 
 export const findByTestAttr = (wrapper, attr) => {
   return wrapper.find(`[data-test='${attr}']`);
@@ -12,4 +16,12 @@ export const checkProps = (component, conformingProps) => {
     component.name
   );
   expect(propError).toBeUndefined();
+};
+
+export const storeFactory = (initialState) => {
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middlewares)
+  );
 };
